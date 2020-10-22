678a0f74ad193edd33c9c29fe838cf5b036c11c0
Rishikesh <53863619+Rishikesh01@users.noreply.github.com>
2020-10-22 20:09:27 +0530

    Add files via upload

---
+++ config/polybar/launch.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+# Terminate already running bar instances
+killall -q polybar
+# If all your bars have ipc enabled, you can also use 
+# polybar-msg cmd quit
+
+# Launch bar1 and bar2
+echo "---" |
+
+polybar music & polybar workspaces & polybar status
+
+
+echo "Bars launched..."
